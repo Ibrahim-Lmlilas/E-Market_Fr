6842f0a9a552b85e81732281d233780a2c7493a1
Ibrahim-Lmlilas <lmlilas.ibrahim@student.youcode.ma>
2025-11-09 13:28:03 +0100

    Initialisation du projet React

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-import AppRoutes from "./routes/AppRoutes";
-import { AuthProvider } from "./context/AuthContext";
 
 const App = () => {
-  return (
-    <AuthProvider>
-      <AppRoutes />
-    </AuthProvider>
-  );
+
 };
 
 export default App;
